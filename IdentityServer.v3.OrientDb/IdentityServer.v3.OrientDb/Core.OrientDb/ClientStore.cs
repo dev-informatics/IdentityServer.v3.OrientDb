4743601d5b0c20d02e804db57e390a61244165be
michael.davidson@devinformatics.com <michael.davidson@devinformatics.com>
2015-06-24 18:21:40 -0500

    Initial commit. Getting ready for active development. Dragged in the Thinktecture dependencies.

---
+++ IdentityServer.v3.OrientDb/IdentityServer.v3.OrientDb/Core.OrientDb/ClientStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Thinktecture.IdentityServer.Core.Models;
+using Thinktecture.IdentityServer.Core.Services;
+
+namespace IdentityServer.v3.OrientDb.Core.OrientDb
+{
+    class ClientStore : IClientStore
+    {
+        public Task<Client> FindClientByIdAsync(string clientId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
